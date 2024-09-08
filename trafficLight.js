ae8e5c3cf677f88655d86886ce2bbdf180968e27
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-09 02:47:24 +0330

    finished

---
+++ trafficLight.js
@@ -0,0 +1,15 @@
+const trafficLightColor = 'GREEN';
+
+
+if(trafficLightColor === 'RED'){
+    console.log('Stop ✋');
+
+}else if(trafficLightColor === 'YELLOW'){
+    console.log('Slow down ☝️');
+
+}else if(trafficLightColor === 'GREEN'){
+    console.log('Go 👋')
+
+}else{
+    console.log('Invalid color 🫤')
+}
