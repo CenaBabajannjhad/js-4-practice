533c6beec6c26e07ec1c9080c5e20fa364c04dda
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-09 03:09:50 +0330

    finished

---
+++ convertScoreToGrade.js
@@ -0,0 +1,27 @@
+const score = 100;
+
+if(score >= 90 && score <= 100){
+    console.log('A+ 😍');
+
+}else if(score <= 89 && score >= 80){
+    console.log('A 🥰');
+
+}else if(score <= 79 && score >= 70){
+    console.log('B+ 🤗');
+
+}else if(score <= 69 && score >= 60){
+    console.log('B 🙂');
+
+}else if(score <= 59 && score >= 50){
+    console.log('C 😐');
+
+}else if(score <= 49 && score >= 0){
+    console.log('D 😶');
+
+}else if(score > 100){
+    console.log('not valid');
+
+}else{
+    console.log('😑👉💩')
+};
+
