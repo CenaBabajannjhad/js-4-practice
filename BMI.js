7f3d03f5134307cf0cfd7d4070918deb66a91510
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-10 18:43:20 +0330

    finished

---
+++ BMI.js
@@ -0,0 +1,20 @@
+let classification;
+let weight = 82;
+let height = 1.93;
+let bmi = weight / (height * height);
+
+
+
+if (bmi < 18.5) {
+  classification = 'Underweight';
+} else if (bmi >= 18.5 && bmi < 25) {
+  classification = 'Normal weight';
+} else if (bmi >= 25 && bmi < 30) {
+  classification = 'Overweight';
+} else {
+  classification = 'Obese';
+}
+
+
+
+console.log(`Your BMI is ${bmi.toFixed(2)} and you are classified as: ${classification}`);
