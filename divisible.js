b96b551e403ede8d463b11d739fcd97c23dd4558
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-15 00:20:19 +0330

    finished

---
+++ divisible.js
@@ -0,0 +1,13 @@
+const divisibleBy3 = [];
+const divisibleBy5 = [];
+const divisibleBy3And5 = [];
+
+for (let i = 1; i <= 45; i++) {
+  if (i % 3 === 0) divisibleBy3.push(i);
+  if (i % 5 === 0) divisibleBy5.push(i);
+  if (i % 3 === 0 && i % 5 === 0) divisibleBy3And5.push(i);
+}
+
+console.log(divisibleBy3);
+console.log(divisibleBy5);
+console.log(divisibleBy3And5);
