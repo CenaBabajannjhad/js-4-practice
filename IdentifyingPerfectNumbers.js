6df95d9c9d2b6639c074c233d547e94be4b53ead
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-16 17:40:20 +0330

    finished

---
+++ IdentifyingPerfectNumbers.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let perfectNumbers = [];
+
+function isPerfectNumber(num) {
+  if (num <= 1) return false;
+
+  let sum = 0;
+
+
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) {
+      sum += i;
+    }
+  }
+
+  
+  if (sum === num) {
+    perfectNumbers.push(num);
+    return true;
+  }
+
+  
+  return false;
+}
