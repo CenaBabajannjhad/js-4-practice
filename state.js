382e674bd9f2d6672eab5660a3d753d68912c742
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-22 00:50:46 +0330

    dev phase

--- state.js
+++ state.js
@@ -1,2 +1,41 @@
-'use strict'
+const formElement = document.querySelector('#form');
+const hourInputElement = document.querySelector('#hour');
+const minInputElement = document.querySelector('#min');
+const hourPlace = document.querySelector('#hour-place');
+const minPlace = document.querySelector('#min-place');
+let hourInputValue , minInputValue;
+// submit event
+formElement.addEventListener('submit' , submitHandler);
 
+
+function submitHandler(e){
+    e.preventDefault();
+
+    // get input values
+    hourInputValue = getInputValue(hourInputElement);
+    minInputValue = getInputValue(minInputElement);
+    // calculate input values
+    setInterval(() => {
+        counterCalc();
+    }, 1000);
+    // reset input values
+    resetInputValue(hourInputElement);
+    resetInputValue(minInputElement);
+}
+
+// counter calc
+function counterCalc(){
+// code...
+}
+// change element textContents
+function changeTextContentNodes(element , value){
+    element.textContent = value;
+}
+// get input value
+function getInputValue(element){
+    return element.value;
+}
+// reset input value
+function resetInputValue(element){
+    element.value = null;
+}
